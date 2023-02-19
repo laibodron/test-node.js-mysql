acfb17481b6faeb9f7aca3f872436fcc8619155d
Денис_Мичкарев <den141delta@gmail.com>
2023-02-20 01:17:13 +0800

    +sql

---
+++ sql/create_users.sql
@@ -0,0 +1,10 @@
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    LastName VARCHAR(255),
+    FirstName VARCHAR(255),
+    FatherName VARCHAR(255),
+    Phone VARCHAR(255),
+    Login VARCHAR(255),
+    Password VARCHAR(255),
+    Email VARCHAR(255)
+);
